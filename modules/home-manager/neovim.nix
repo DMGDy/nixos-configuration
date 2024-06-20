3b2446c1e1961fbc97e6147e16d1b7825c840368
DMGDy <dylangarza1909@gmail.com>
2024-06-20 01:59:34 -0400

    made propper home-manager modules for zsh and neovim

---
+++ modules/home-manager/neovim.nix
@@ -0,0 +1,32 @@
+{ pkgs, config, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    extraConfig = ''
+      syntax on
+      set number
+      set relativenumber
+      let g:airline_theme='deus'
+      set tabstop=2
+      set shiftwidth=2
+      set softtabstop=2
+      set expandtab
+      
+      let g:indentLine_char='|'
+    '';
+
+    plugins = with pkgs.vimPlugins; [
+      YouCompleteMe
+      nerdtree
+      nvim-autopairs
+      vim-airline
+      vim-airline-themes
+      vim-nix
+      pretty-fold-nvim
+      indentLine
+      markdown-preview-nvim
+    ];
+  };
+}
---
+++ modules/home-manager/zsh.nix
@@ -0,0 +1,40 @@
+{ pkgs, config, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+
+    dotDir = ".zshdir/";
+    autosuggestion.enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ls = "ls --color=auto";
+      shutdown = "sudo shutdown -h now";
+    };
+
+    history = {
+      size  = 50000;
+      path = "/home/dylandy/.zshdir/.zsh_history";
+    };
+
+    initExtraFirst = ''
+        pfetch
+        VI_MODE_SET_CURSOR=true
+        wallust cs /home/dylandy/.cache/wallust/Full/Lab/SoftDarkComp/20/nixos-anime.png_3159858_84823779_1.3_I.json 1> /dev/null
+    '';
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "colored-man-pages"
+        "fzf"
+        "vi-mode"
+      ];
+      custom = "/home/dylandy/.zshdir/oh-my-zsh/themes";
+      theme = "agnoster_custom";
+
+    };
+  };
+}
