2c1dfe6a34cde27d8afab7709c3b6d92e02c6b68
DMGDy <dylangarza1909@gmail.com>
2024-09-06 16:41:59 -0400

    hyprland.nix: configured to mirror display default

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -21,7 +21,10 @@ in
         gaps_out = "0";
       };
       # monitor scaling down since default is 2
-      monitor = "eDP-1,2256x1504@59.999,0x0,1.175,bitdepth, 10";
+      monitor = [
+        "eDP-1,2256x1504@59.999,0x0,1.175,bitdepth, 10"
+        ",preferred,auto,1,mirror,eDP-1"
+      ];
 
       "$mod" = "Alt";
       "debug:disable_logs" = "false";
