9e67b8ae408931bcccbe93e5798d523a547a9cec
DMGDy <dylangarza1909@gmail.com>
2024-09-18 09:59:04 -0400

    display-manager.nix: using ly as dm

--- modules/nixos/display-manager.nix
+++ modules/nixos/display-manager.nix
@@ -1,18 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  services.displayManager.sddm = {
+  servies.displayManager.ly = {
     enable = true;
-    enableHidpi = true;
-    wayland.enable = true;
-    theme = "where_is_my_sddm_theme";
-    settings = {
-      Item = {
-        id = "cursor";
-        visible = "false";
-      };
-    };
-
-    autoLogin.relogin = true;
   };
+#  services.displayManager.sddm = {
+#    enable = true;
+#    enableHidpi = true;
+#    wayland.enable = true;
+#    theme = "where_is_my_sddm_theme";
+#    settings = {
+#      Item = {
+#        id = "cursor";
+#        visible = "false";
+#      };
+#    };
+#
+#    autoLogin.relogin = true;
+#  };
+
 }
+
