db04498ae55cb5c4891c06fe25bb63f51d17db4a
DMGDy <dylangarza1909@gmail.com>
2024-10-04 15:17:28 -0400

    tmux: changing switching pane bindings to vim like keys

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -9,5 +9,39 @@
     shortcut = "a";
     terminal = "screen-256color";
     resizeAmount = 1;
+
+    extraConfig = "
+      set -g status-position bottom
+      set -g status-justify left
+
+      # show date and time
+      set -g status-left ''
+      set -g status-right '%d-%m-%Y %H:%M'
+
+      # colors active tags 
+      setw -g window-status-current-style 'fg=white bg=magenta'
+      setw -g window-status-current-format ' #I #W #F '
+
+
+
+
+      # unbind arrow keys and pane switching keys
+
+      unbind Up     
+      unbind Down   
+      unbind Left   
+      unbind Right  
+
+      unbind C-Up   
+      unbind C-Down 
+      unbind C-Left 
+      unbind C-Right
+
+      # bind vim keys to moving around
+      bind -r k select-pane -U 
+      bind -r j select-pane -D 
+      bind -r h select-pane -L 
+      bind -r l select-pane -R
+    ";
   };
 }
