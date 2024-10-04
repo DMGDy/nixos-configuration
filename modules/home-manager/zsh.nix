63323aa765e998f75c8661e45bb2fcd99fd43cac
DMGDy <dylangarza1909@gmail.com>
2024-10-04 17:55:18 -0400

    zsh: autosuggestion currently not working for new sessions

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -19,10 +19,13 @@
       size  = 50000;
       path = "/home/dylandy/.zshdir/.zsh_history";
     };
+    historySubstringSearch.enable = true;
 
     initExtra= ''
         VI_MODE_SET_CURSOR=true
-        DISABLE_FZF_AUTO_COMPLETION="false"
+        bindkey '^[[A' history-substring-search-up # or '\eOA'
+        bindkey '^[[B' history-substring-search-down # or '\eOB'
+        HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
     '';
 
     initExtraFirst = ''
@@ -50,6 +53,7 @@
         "zpm-zsh/pr-user"
         "unixorn/fzf-zsh-plugin"
         "Aloxaf/fzf-tab"
+        "zsh-users/zsh-history-substring-search"
       ];
     };
 
