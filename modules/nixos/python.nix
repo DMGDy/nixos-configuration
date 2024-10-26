f2f8e02e44405b894caad9e8d0fe33872f622e13
DMGDy <dylangarza1909@gmail.com>
2024-10-26 18:28:28 -0400

    [python] various python packages (numpy panads matplotlob sklearn

--- modules/nixos/python.nix
+++ modules/nixos/python.nix
@@ -4,6 +4,10 @@
   environment.systemPackages = with pkgs; [
     (python3.withPackages(ps: with ps; [
       psutil
+      numpy
+      pandas
+      matplotlib
+      sklearn-deap
     ]))
   ];
 }
