30f25a1dba757234dd509414c0ff396d6054fdc7
DMGDy <dylangarza1909@gmail.com>
2024-10-31 13:03:22 -0400

    [python] dont need

--- modules/nixos/python.nix
+++ modules/nixos/python.nix
@@ -4,11 +4,6 @@
   environment.systemPackages = with pkgs; [
     (python3.withPackages(ps: with ps; [
       psutil
-      numpy
-      pandas
-      matplotlib
-      sklearn-deap
-      scikit-learn
     ]))
   ];
 }
