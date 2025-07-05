5cc87eb91f6cc7790600df02be60b1ba57fe9a43
DMGDy <dylangarza1909@gmail.com>
2025-07-05 00:15:16 -0400

    framework/home: local zsh configuration

--- hosts/framework/home.nix
+++ hosts/framework/home.nix
@@ -5,7 +5,6 @@
     inputs.stylix.homeManagerModules.stylix
     ./../../modules/home-manager/tofi.nix
     ./../../modules/home-manager/hyprland.nix
-    ./../../modules/home-manager/zsh.nix
     ../../modules/home-manager/stylix.nix
 #    ../../modules/home-manager/eww.nix
     ../../modules/home-manager/tmux.nix
@@ -30,12 +29,66 @@
       download = "$~/Downloads";
     };
   };
+
+  programs.zsh = {
+    enable = true;
+
+    dotDir = ".zshdir/";
+    autosuggestion.enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = false;
+
+    shellAliases = {
+      ls = "ls --color=auto";
+      shutdown = "sudo shutdown -h now";
+      nix-reflake= "nix-reflake() { sudo nixos-rebuild switch --flake .#$1 };nix-reflake";
+    };
+
+    history = {
+      share = true;
+      size  = 50000;
+      path = "/home/dylandy/.zshdir/.zsh_history";
+      ignoreDups = true;
+      expireDuplicatesFirst = true;
+    };
+    historySubstringSearch.enable = true;
+
+    initExtra= ''
+      VI_MODE_SET_CURSOR=true
+      bindkey '^[[A' history-substring-search-up # or '\eOA'
+      bindkey '^[[B' history-substring-search-down # or '\eOB'
+      HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+      '';
+
+    initExtraFirst = ''
+      setopt PROMPT_SUBST
+      setopt norcs
+
+# PROMPT='%F{white}┌%f%F{white}[%f%F{magenta}%n%f%F{white}@%f%F{cyan}%m%f%F{white}]%f%F{8}::%f%F{white}[%f%F{blue}%~%f%F{white}]%f%F{8}::$(git_super_status) 
+      PROMPT='%F{white}┌%f%F{white}[%f%F{magenta}%n%f%F{white}@%f%F{cyan}%m%f%F{white}]%f%F{8}::%f%F{white}[%f%F{blue}%~%f%F{white}]%f%F{8}
+    %F{white}└%f%F{white}[%f%F{yellow}$%f%F{white}]%f%F{green}❯%f '
+
+# Right prompt for additional information (e.g., time)
+      RPROMPT='%F{green}$pr_return%f'
+      pfetch
+      '';
+
+    antidote = {
+      enable = true;
+      plugins = [
+        "jeffreytse/zsh-vi-mode"
+          "zpm-zsh/colors"
+          "zpm-zsh/figures"
+          "zsh-users/zsh-autosuggestions"
+          "zsh-users/zsh-syntax-highlighting"
+          "unixorn/fzf-zsh-plugin"
+          "Aloxaf/fzf-tab"
+          "zsh-users/zsh-history-substring-search"
+      ];
+    };
+
+  };
   home.stateVersion = "24.05"; # Please read the comment before changing.
-
-  home.packages = [
-     pkgs.hello
-
-  ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
