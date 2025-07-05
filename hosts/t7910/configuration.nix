be31a6645b800823cff1494b4224909ac7a1b2c5
Dylan Dy <dylanmatthew.garza@resideo.com>
2025-07-04 21:36:22 -0400

    t7910/configuration: kernel 6.6, zram, ip routing, swap

--- hosts/t7910/configuration.nix
+++ hosts/t7910/configuration.nix
@@ -7,17 +7,41 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    ./hardware-configuration.nix
       inputs.home-manager.nixosModules.home-manager
       ../../modules/nixos/display-manager.nix
       ../../modules/nixos/package-config/openssh.nix
       ../../modules/nixos/packages2.nix
       ../../modules/nixos/fonts.nix
       ../../modules/nixos/environment.nix
-      ../../modules/nixos/nginx.nix
       ../../modules/nixos/python.nix
-      ../../modules/nixos/avahi.nix
+      ../../modules/nixos/docker.nix
     ];
+  swapDevices = [ {
+      device = "/swapfile";
+      size = 64 * 1024;
+    }
+    {
+      device = "/swapfile2";
+      size = 128 * 1024;
+    }
+  ];
+  zramSwap= {
+    enable = true;
+    memoryMax = 65536;
+    memoryPercent = 50;
+    writebackDevice = "/dev/zvol/tank/swap";
+  };
+
+  programs.nix-ld.enable = true;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+
+  boot.kernelPackages = pkgs.linuxPackages_6_6;
 
   boot.loader = {
     efi = {
@@ -26,38 +50,116 @@
     systemd-boot.enable = true;
   };
   networking = {
-    # DNS settings
+    networkmanager.enable = true;  
     nameservers = [ "10.94.2.200" "10.86.2.200" ];
-    
-    # Static routes
-    networkmanager.enable = true;  # If you're using NetworkManager
-    
-    # Static route configuration
-    extraHosts = ''
-      # Any host mappings you might need
-    '';
-    
-    # Define the routes from the image
-    staticRoutes = [
-      { destination = "10.8.0.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "10.10.30.21"; netmask = "255.255.255.255"; gateway = "158.100.69.1"; }
-      { destination = "10.10.32.86"; netmask = "255.255.255.255"; gateway = "158.100.69.1"; }
-      { destination = "10.59.0.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "10.70.0.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "10.86.2.200"; netmask = "255.255.255.255"; gateway = "158.100.69.1"; }
-      { destination = "10.94.2.200"; netmask = "255.255.255.255"; gateway = "158.100.69.1"; }
-      { destination = "10.122.0.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "10.192.0.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "10.194.0.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "10.216.0.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "158.100.0.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "158.100.77.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "159.99.0.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "165.195.0.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "172.26.0.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "199.63.0.0"; netmask = "255.255.0.0"; gateway = "158.100.69.1"; }
-      { destination = "158.100.69.1"; netmask = "255.255.255.255"; gateway = "158.100.69.1"; }
-      { destination = "199.63.86.148"; netmask = "255.255.255.255"; gateway = "158.100.69.1"; }
+
+    interfaces.enp9s0.ipv4.routes = [
+    {
+      address      = "10.8.0.0";
+      prefixLength = 16;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "10.10.30.21";
+      prefixLength = 32;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "10.10.32.86";
+      prefixLength = 32;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "10.59.0.0";
+      prefixLength = 16;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "10.70.0.0";
+      prefixLength = 16;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "10.86.2.200";
+      prefixLength = 32;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "10.94.2.200";
+      prefixLength = 32;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "10.122.20.0";
+      prefixLength = 24;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "10.192.0.0";
+      prefixLength = 16;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "10.194.0.0";
+      prefixLength = 16;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "10.216.0.0";
+      prefixLength = 16;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "158.100.0.0";
+      prefixLength = 32;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "158.100.67.0";
+      prefixLength = 24;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "158.100.67.1";
+      prefixLength = 32;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "158.100.77.0";
+      prefixLength = 26;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "159.99.0.0";
+      prefixLength = 16;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "165.195.0.0";
+      prefixLength = 26;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "172.26.0.0";
+      prefixLength = 16;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "199.63.0.0";
+      prefixLength = 16;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "158.100.89.254";
+      prefixLength = 32;
+      via          = "158.100.89.254";
+    }
+    {
+      address      = "199.63.86.148";
+      prefixLength = 32;
+      via          = "158.100.89.254";
+    }
+
     ];
   };
 
@@ -65,47 +167,46 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   networking.hostName = "ny80t7910"; # Define your hostname.
-  # Pick only one of the below networking options.
+# Pick only one of the below networking options.
 
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
-  time.timeZone = "America/New_York";
+# Set your time zone.
+    time.timeZone = "America/New_York";
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-  # Enable touchpad support (enabled default in most desktopManager).
+# Enable CUPS to print documents.
+# services.printing.enable = true;
+# Enable touchpad support (enabled default in most desktopManager).
 
   services.blueman.enable = true;
   programs.zsh.enable = true;
   nixpkgs.config.allowUnfree = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.dylandy= {
-     shell = pkgs.zsh;
-     isNormalUser = true;
-     extraGroups = [ "wheel"
-       "sudo" 
-       "input" 
-       "networkmanager" 
-     ];
-   };
-   home-manager = {
-     extraSpecialArgs = { inherit inputs; };
-     users = {
-       dylandy = import ./home.nix;
-     };
-   };
+# Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dylandy= {
+    shell = pkgs.zsh;
+    isNormalUser = true;
+    extraGroups = [ "wheel"
+      "sudo" 
+      "input" 
+      "networkmanager" 
+    ];
+  };
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      dylandy = import ./home.nix;
+    };
+  };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-   environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-     neofetch
-     git
-     tree
-   ];
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      wget
+      neofetch
+      git
+      tree
+  ];
 
   programs.hyprland = {
     enable = true;
@@ -125,7 +226,7 @@
   };
 
   xdg.portal.enable = true;
-  
+
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
