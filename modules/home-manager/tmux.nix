72de9cbf9bd1209677a22fecdba2612d984a1585
Dylan Dy <dylanmatthew.garza@resideo.com>
2025-07-18 18:38:48 -0400

    tmux: added extrakto plugin and status themes to bar

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -9,6 +9,7 @@
     shortcut = "a";
     terminal = "screen-256color";
     resizeAmount = 1;
+    baseIndex = 1;
 
     extraConfig = "
       set -g @plugin 'tmux-plugins/tmux-resurrect'
@@ -16,18 +17,14 @@
       set -g @plugin 'tmux-plugins/tmux-yank'
       set -g @plugin 'tmux-plugins/tmux-sidebar'
       set -g @plugin 'rose-pine/tmux'
+      set -g @plugin 'laktak/extrakto'
       set -g @resurrect-capture-pane-contents 'on'
       set -g @resurrect-strategy-nvim 'session'
 
-
       set -g status-position bottom
       set -g status-justify left
-      # show date and time
       set -g status-left ''
-      set -g status-right '%d-%m-%Y %H:%M'
-
-      # colors active tags 
-      setw -g window-status-current-format ' #I #W #F '
+      set -g status-right
 
       # start windows and panes at 1, not 0 set -g base-index 1
       setw -g pane-base-index 1
@@ -55,20 +52,29 @@
       bind -r C-h resize-pane -L
       bind -r C-l resize-pane -R     
 
+      set -g history-limit 131072
+
     ";
     plugins = with pkgs.tmuxPlugins; [
      resurrect
      tmux-floax
      yank
      sidebar
+     extrakto
      {
+
       plugin = rose-pine;
       extraConfig = ''
-        set -g @rose_pine_variant 'main' # Options are 'main', 'moon' or 'dawn'
-        set -g @rose_pine_left_separator ' > ' # The strings to use as separators are 1-space padded
-        set -g @rose_pine_right_separator ' < ' # Accepts both normal chars & nerdfont icons
-        set -g @rose_pine_field_separator ' | ' # Again, 1-space padding, it updates with prefix + I
-        set -g @rose_pine_window_separator ' - ' # Replaces the default `:` between the window number and name
+        set -g @rose_pine_variant 'main'
+        set -g @rose_pine_host 'on' # Enables hostname in the status bar
+        set -g @rose_pine_date_time '%d-%m-%Y %H:%M' # It accepts the date UNIX command format (man date for info)
+        set -g @rose_pine_user 'on' # Turn on the username component in the statusbar
+        set -g @rose_pine_directory 'on' # Turn on the current folder component in the status bar
+        set -g @rose_pine_bar_bg_disabled_color_option 'default'
+
+        set -g @rose_pine_default_window_behavior 'on' # Forces tmux default window list behaviour
+        set -g @rose_pine_show_current_program 'on' # Forces tmux to show the current running program as window name
+        set -g @rose_pine_show_pane_directory 'on' # Forces tmux to show the current directory as window name
 
         set -g @rose_pine_session_icon '' # Changes the default icon to the left of the session name
         set -g @rose_pine_current_window_icon '' # Changes the default icon to the left of the active window name
@@ -76,6 +82,7 @@
         set -g @rose_pine_username_icon '' # Changes the default icon to the right of the hostname
         set -g @rose_pine_hostname_icon '󰒋' # Changes the default icon to the right of the hostname
         set -g @rose_pine_date_time_icon '󰃰' # Changes the default icon to the right of the date module
+        set -g @rose_pine_window_status_separator "  " # Changes the default icon that appears between window namesk
       '';
      }
     ];
