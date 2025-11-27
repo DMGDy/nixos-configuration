db0acc0c07876d4b1972329bf4b420d12b7a5307
Dylan Dy <dylanmatthew.garza@resideo.com>
2025-11-26 20:35:07 -0500

    bad nvidia drivers

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -4,16 +4,24 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+
+    extraPackages = with pkgs; [
+      intel-media-driver
+        vaapiIntel
+        vaapiVdpau
+        libvdpau
+    ];
   };
   
-
   services.xserver.videoDrivers = [ "nvidia"  ];
 
   hardware.nvidia = {
     open = false;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
 
     modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
   };
   
   boot.kernelParams = lib.mkMerge [
@@ -21,4 +29,12 @@
   ];
 
   boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"  ];
+  environment.sessionVariables= {
+    GBM_BACKEND= "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    WLR_NO_HARDWARE_CURSORS = "1";
+
+    __NV_PRIME_RENDER_OFFLOAD = "0";
+    LIBVA_DRIVER_NAME = "nvidia";
+  };
 }
