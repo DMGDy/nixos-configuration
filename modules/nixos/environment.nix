d1f71c250775bafe09f5b6af351230a8498438f6
Dylan Dy <dylanmatthew.garza@resideo.com>
2026-09-09 16:00:22 -0400

    environment: add project directory shortcut env vars

--- modules/nixos/environment.nix
+++ modules/nixos/environment.nix
@@ -4,5 +4,14 @@
   environment.variables = {
     PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
     PATH = "$PATH:$HOME/Scripts";
+
+    # Project directory shortcuts, all located under $HOME
+    QUICK = "$HOME/QUICK";
+    GRIP = "$HOME/GRIP";
+    ADT = "$HOME/ADT";
+    GOP = "$HOME/GOP";
+    TS = "$HOME/TS";
+    RSW = "$HOME/RSW";
+    OTHER = "$HOME/Other";
   };
 }
