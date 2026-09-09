836d5a3b28c18236efc80b85ee80ce3bb0241f74
Dylan Dy <dylanmatthew.garza@resideo.com>
2026-09-09 16:00:22 -0400

    tomcat: configure port, user, package, and opengrok webapp

--- modules/nixos/tomcat.nix
+++ modules/nixos/tomcat.nix
@@ -1,4 +1,14 @@
-{ ...}:
+{ pkgs, ...}:
 {
   services.tomcat.enable = true;
+  
+  services.tomcat.port = 9090;
+
+  services.tomcat.user = "dylandy";
+
+  services.tomcat.package = pkgs.tomcat10;
+
+  services.tomcat.webapps = [ 
+    /var/www/html/opengrok/dist/lib/source.war
+  ];
 }
