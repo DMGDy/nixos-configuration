7da2899d196dab7df6809232b13a0dcb5354f826
Dylan Dy <dylanmatthew.garza@resideo.com>
2026-09-09 16:00:22 -0400

    framework_desktop: parameterize home.nix by username

--- hosts/framework_desktop/home.nix
+++ hosts/framework_desktop/home.nix
@@ -1,3 +1,4 @@
+{ username ? "dylandy" }:
 { config, pkgs, inputs, lib, ... }:
 
 {
@@ -14,10 +15,10 @@
   ];
 
   stylix.enable = true;
-  home.username = "dylandy";
-  home.homeDirectory = "/home/dylandy";
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
   home.preferXdgDirectories = true;
-  home.sessionPath = 
+  home.sessionPath =
     [
       "$HOME/.cargo/bin/"
       "$HOME/Scripts/"
@@ -25,7 +26,7 @@
     ];
 
   xdg = {
-    configHome = /home/dylandy/.config;
+    configHome = "/home/${username}/.config";
     userDirs = {
       enable = true;
       setSessionVariables = true;
